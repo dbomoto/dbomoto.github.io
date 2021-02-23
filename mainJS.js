c1a5ebace64765f684e0114be2c3cdc4a0177405
Daniel Benedict L. Omoto <78993631+dbomoto@users.noreply.github.com>
2021-02-23 23:51:52 +0800

    Inserted knob1 to document
    Animation still in progress.

--- mainJS.js
+++ mainJS.js
@@ -1,2 +1,15 @@
-﻿/*eslint-env browser*/
-/*this tells eslint that the environment is a browser*/
+﻿/*eslint-env es6*/
+/*eslint-env browser*/
+var knob1 = document.getElementsByClassName('knob-1')[0];
+var knob1Counter = 0;
+window.alert(knob1);
+
+function knobRotate() {
+	knob1Counter += 45;
+	if (knob1Counter == 360) {
+		knob1Counter = 0;
+	}
+	knob1.style.transform = `rotate(${knob1Counter}deg)`;
+}
+
+knob1.addEventListener('click',function(){window.requestAnimationFrame(knobRotate)})
