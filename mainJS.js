b2e489e2a3924504f59deedb98507a7c1cae54fe
Daniel Benedict L. Omoto <78993631+dbomoto@users.noreply.github.com>
2021-02-24 23:29:40 +0800

    Finished Knob-2
    Added media queries for knob-2, new resources, and basic animation. Ready for data implementation.

--- mainJS.js
+++ mainJS.js
@@ -1,9 +1,10 @@
 ﻿/*eslint-env es6*/
 /*eslint-env browser*/
 window.onload = function(){
+	
+/*animation for knob-1*/
 var knob1 = document.getElementsByClassName('knob-1')[0];
 var knob1Counter = 0;
-
 function knobRotate() {
 	knob1Counter += 45;
 	if (knob1Counter == 360) {
@@ -11,7 +12,18 @@ function knobRotate() {
 	}
 	knob1.style.transform = `rotate(${knob1Counter}deg)`;
 }
-
 knob1.addEventListener('click',function(){window.requestAnimationFrame(knobRotate)})
+
 	
+/*animation for knob-2*/	
+var knob2 = document.getElementsByClassName('knob-2')[0];
+var knob2Counter = 0;
+function knobRotate2() {
+	knob2Counter += 45;
+	if (knob2Counter == 360) {
+		knob2Counter = 0;
+	}
+	knob2.style.transform = `rotate(${knob2Counter}deg)`;
+}
+knob2.addEventListener('click',function(){window.requestAnimationFrame(knobRotate2)})	
 }
