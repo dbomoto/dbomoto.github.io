4116a309c69c0c064cf936c8e526ea487df5d97b
Daniel Benedict L. Omoto <78993631+dbomoto@users.noreply.github.com>
2021-02-27 23:51:04 +0800

    Added container for other Pages content
    Pages knob know will now change crt content.

--- mainJS.js
+++ mainJS.js
@@ -12,7 +12,8 @@ function knobRotate() {
 	}
 	knob1.style.transform = `rotate(${knob1Counter}deg)`;
 }
-knob1.addEventListener('click',function(){window.requestAnimationFrame(knobRotate)})
+knob1.addEventListener('click',function(){window.requestAnimationFrame(knobRotate)});
+
 
 	
 /*animation for knob-2*/	
@@ -26,4 +27,25 @@ function knobRotate2() {
 	knob2.style.transform = `rotate(${knob2Counter}deg)`;
 }
 knob2.addEventListener('click',function(){window.requestAnimationFrame(knobRotate2)})	
+	
+/*changes the display in the crt as Pages knob is clicked*/	
+var crtPages = document.getElementsByClassName('crt-pages');
+function crtPagesToggle() {
+	let toShow=0;
+	for(let x = 0; x <= crtPages.length-1; x++){
+		if (!crtPages[x].classList.contains('hide')){
+			toShow = x;
+		}
+	}
+	if (toShow == crtPages.length-1) {
+		crtPages[toShow].classList.toggle('hide');
+		crtPages[0].classList.toggle('hide');
+	} else {
+		crtPages[toShow].classList.toggle('hide');
+		crtPages[toShow + 1].classList.toggle('hide');		
+	}
+}
+knob1.addEventListener('click', crtPagesToggle);
+
+	
 }
