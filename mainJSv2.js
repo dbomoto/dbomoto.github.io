c9ca02b8a8f6b01598fb3d044c439c96a30a62b9
Daniel Benedict L. Omoto <78993631+dbomoto@users.noreply.github.com>
2021-03-29 23:52:57 +0800

    Redo some code to keep DRY
    Adding comments where necessary also for CSS

--- mainJSv2.js
+++ mainJSv2.js
@@ -3,7 +3,13 @@ window.onload = function(){
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
     
+    // aspect ratio of 14:9
 
+    vw / 14
+    vh / 9
+
+    // pick whichever is lower, and floor it, use it as the base multiplier for 
+    // the aspect ratio
 
 
 
