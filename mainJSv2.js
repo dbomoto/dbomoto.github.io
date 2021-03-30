4f4fbc904569361a5ee0ead3fc9097d45eee1666
Daniel Benedict L. Omoto <78993631+dbomoto@users.noreply.github.com>
2021-03-31 02:45:19 +0800

    Made tv background responsive according to aspect ratio
    Tv background is now responsive and displays in the correct aspect ratio.
    Temporarily disabled many HTML elements and CSS classes to test easier.

--- mainJSv2.js
+++ mainJSv2.js
@@ -1,15 +1,35 @@
 window.onload = function(){
     
-    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    var vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
     
     // aspect ratio of 14:9
 
-    vw / 14
-    vh / 9
+    var widthMax = vw / 14
+    var heightMax = vh / 9
+
+    var tvMultiplier = 1;
+
+    if (widthMax > heightMax){
+        tvMultiplier = Math.floor(heightMax);
+    } else {
+        tvMultiplier = Math.floor(widthMax);
+    }
+
+    var tvBackground = document.querySelectorAll(".background")
+
+    
+
+    tvBackground[0].style.width = `${14 * tvMultiplier}px`;
+    tvBackground[0].style.height = `${9 * tvMultiplier}px`;
+
 
     // pick whichever is lower, and floor it, use it as the base multiplier for 
     // the aspect ratio
+
+    // make it into a function that will be reusable. accepts the element chosen,
+    // outputs the corresponding widht and height.
 
 
 
